c4ce7eae69fc6021c508b053a088bad420b2e609
Matt Parsons <parsonsmatt@gmail.com>
2016-04-04 23:05:26 -0400

    add some test stuff

--- test/Main.purs
+++ test/Main.purs
@@ -1,8 +1,13 @@
 module Test.Main where
 
-import Prelude (Unit)
+import Prelude
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+
+import Test.QuickCheck.Laws.Data.Functor (checkFunctor)
+
+import Data.Zipper.Tree (TreeZipper(..))
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
